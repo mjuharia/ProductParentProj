91c32b360645ad1c1120a788d063ddfd809d4e30
mjuharia <movses.juharian@gmail.com>
2022-09-13 14:13:56 -0400

    updated parent, chiled pom files and implemented createProductServlet

--- productWeb/src/main/java/com/moso/product/servlets/CreateProductServlet.java
+++ productWeb/src/main/java/com/moso/product/servlets/CreateProductServlet.java
@@ -6,6 +6,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.moso.product.bo.ProductBO;
+import com.moso.product.bo.ProductBOImpl;
+import com.moso.product.dto.Product;
+
 /**
  * Servlet implementation class CreateProductServlet
  */
@@ -17,7 +21,7 @@ public class CreateProductServlet extends HttpServlet {
      */
     public CreateProductServlet() {
         super();
-        // TODO Auto-generated constructor stub
+        
     }
 
 	/**
@@ -32,9 +36,19 @@ public class CreateProductServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		
-		doGet(request, response);
+		int id = Integer.parseInt(request.getParameter("id"));
+		String name = request.getParameter("name");
+		String description = request.getParameter("description");
+		int price = Integer.parseInt(request.getParameter("price"));
+		ProductBO myBO = new ProductBOImpl();
+		Product prd = new Product();
+		prd.setId(id);
+		prd.setDescription(description);
+		prd.setName(name);
+		prd.setPrice(price);
+		myBO.create(prd);
+		//doGet(request, response);
 	}
 
 }
